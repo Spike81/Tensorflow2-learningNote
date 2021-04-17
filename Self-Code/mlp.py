cd417daa55a3da6f83f655edb8c933f4c253ad39
Spike81 <52449420+Spike81@users.noreply.github.com>
2021-04-17 21:25:58 +0800

    commit

--- Self-Code/mlp.py
+++ Self-Code/mlp.py
@@ -1,8 +1,8 @@
 # !/usr/local/bin/python3
-# @Time : 2021/3/8 13:57
+# @Time : 2021/4/17 17:17
 # @Author : Tianlei.Shi
 # @Site :
-# @File : tensor.py
+# @File : mlp.py
 # @Software : PyCharm
 
 import os
@@ -16,9 +16,18 @@ tf.config.experimental.set_virtual_device_configuration(
     [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)]
 )
 
-import numpy as np
-from math import e
+from tensorflow import keras
 
-x = tf.random.normal([256])
-net = tf.keras.layers.Dense(x)
-net.build(input_shape=(None, 20))
+x = tf.random.normal([2,3])
+
+model = keras.Sequential([
+    keras.layers.Dense(2, activation="relu"),
+    keras.layers.Dense(2, activation="relu"),
+    keras.layers.Dense(2)
+])
+
+model.build(input_shape=[None, 3])  # 这里的 3，代表 w.shape[0]
+model.summary()
+
+for p in model.trainable_variables:
+    print(p.name, p.shape)
